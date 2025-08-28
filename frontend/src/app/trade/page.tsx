d32f3a7b1e91c30d8f308ae0b1443ad0e2ae478b
Abhyuday <abhyuday911@gmail.com>
2025-08-28 16:20:45 +0530

    feat: frontend inicialization and basic candles implementation

---
+++ frontend/src/app/trade/page.tsx
@@ -0,0 +1,13 @@
+import CandleChart from '@/components/CandleChart'
+import React from 'react'
+
+const page = () => {
+
+  return (
+    <div className='p-4 w-screen h-screen'>
+        <CandleChart/>
+    </div>
+  )
+}
+
+export default page
